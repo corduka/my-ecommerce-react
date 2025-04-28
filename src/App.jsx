69280a084de64f93e1f53c6a05314d7b87670596
corduka <cordukabdullah@gmail.com>
2025-04-28 11:02:39 -0500

    PreEntrega1+Corduk

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greeting="Welcome to MyStore!" />
+    </>
+  );
+}
+
+export default App;
